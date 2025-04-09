25a00c5e06792fe834faec7c97e66cda2b65135f
kavinfidel <145898513+kavinfidel@users.noreply.github.com>
2025-04-09 14:23:00 +0530

    modified:   PrepareDatasets.py

---
+++ # Code Citations.md
@@ -0,0 +1,17 @@
+# Code Citations
+
+## License: GPL_3_0
+https://github.com/diningphil/gnn-comparison/tree/264491c6de1044286d86d19bd2848c23617c2ef7/PrepareDatasets.py
+
+```
+: RedditBinary,
+    'REDDIT-MULTI-5K': Reddit5K,
+    'COLLAB': Collab,
+    'IMDB-BINARY': IMDBBinary,
+    'IMDB-MULTI': IMDBMulti,
+    'NCI1': NCI1,
+    'ENZYMES': Enzymes,
+    'PROTEINS': Proteins,
+    'DD
+```
+
--- PrepareDatasets.py
+++ PrepareDatasets.py
@@ -15,9 +15,15 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 import argparse
-
+import os
+import logging
+import torch
 from datasets import *
+from datasets.graph import Graph
+from torch_geometric.datasets import TUDataset
+from torch_geometric_data import Data
 
+logging.basicConfig(level = logging.INFO, format ='%(asctime)s - %(levelname)s - %(message)s' )
 DATASETS = {
     'REDDIT-BINARY': RedditBinary,
     'REDDIT-MULTI-5K': Reddit5K,
@@ -48,25 +54,72 @@ def get_args_dict():
                         default=False, help='use degree as feature')
     parser.add_argument('--no-kron', dest='precompute_kron_indices', action='store_false',
                         default=True, help='don\'t precompute kron reductions')
+    parser.add_argument('--use-rewired', action = 'store_true', 
+                        default = False, help = 'Add rewired edges to the dataset.')
 
     return vars(parser.parse_args())
 
 
-def preprocess_dataset(name, args_dict):
-    dataset_class = DATASETS[name]
-    if name == 'ENZYMES':
-        args_dict.update(use_node_attrs=True)
-    dataset_class(**args_dict)
+# def preprocess_dataset(name, args_dict):
+#     dataset_class = DATASETS[name]
+#     if name == 'ENZYMES':
+#         args_dict.update(use_node_attrs=True)
+#     dataset_class(**args_dict)
+
+def preprocess_dataset(dataset_path, dataset_name, use_rewired = False):
+
+    """
+    Preprocesses the dataset and rewires them
+    """
+    dataset_dir = os.path.join(dataset_path, dataset_name)
+    if not os.path.exists(dataset_dir):
+        raise FileNotFoundError(f"{dataset_dir} does not exist")
+
+    logging.info(f"Preprocessing started for {dataset_name}")
+    if dataset_name in DATASETS:
+        dataset_class = DATASETS[dataset_name]
+
+        if dataset_name == 'ENZYMES':
+            dataset = dataset_class(root = dataset_path, use_node_attr = True)
+            logging.info(f"Additional 18 node attributes used for ENZYMES")
+        else:
+            dataset = dataset_class(root = dataset_path)
+    else:
+        dataset = TUDataset(root = dataset_path, name = dataset_name)
+
+    for data in dataset:
+        graph = Graph(target =  data.y.item())
+        logging.info(f"Processing graph {i+1}/{len(dataset)}")
+        graph.add_edges_from(data.edge_index.t().tolist())
+    
+        if use_rewired:
+            rewired_edge_index = graph.rewire_edges()
+            data.rewired_edge_index = rewired_edge_index
+
+    torch.save(dataset, os.path.join(dataset_path, f"{dataset_name}_processed.pt"))
+    print(f"Dataset {dataset_name} proccesed & saved {dataset_path}.")
+
 
 
 if __name__ == "__main__":
-    args_dict = get_args_dict()
+    
+    # args_dict = get_args_dict()
 
-    print(args_dict)
+    # print(args_dict)
 
-    dataset_name = args_dict.pop('dataset_name')
-    if dataset_name == 'all':
-        for name in DATASETS:
-            preprocess_dataset(name, args_dict)
-    else:
-        preprocess_dataset(dataset_name, args_dict)
+    # dataset_name = args_dict.pop('dataset_name')
+    # if dataset_name == 'all':
+    #     for name in DATASETS:
+    #         preprocess_dataset(name, args_dict)
+    # else:
+    #     preprocess_dataset(dataset_name, args_dict)
+
+    parser = argparse.ArgumentParser(description="Preprocess datasets.")
+    parser.add_argument("dataset_path", type=str, help="Path to the dataset folder.")
+    parser.add_argument("--dataset-name", type=str, required=True, help="Name of the dataset.")
+    parser.add_argument("--use-rewired", action="store_true", help="Add rewired edges to the dataset.")
+    args = parser.parse_args()
+    
+    preprocess_dataset(args.dataset_path, args.dataset_name, use_rewired=args.use_rewired)
+
+# use it like: python PrepareDatasets.py DATA/CHEMICAL --dataset-name NCI1 --use-rewired
